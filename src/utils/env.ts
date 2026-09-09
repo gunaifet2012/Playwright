c3bf330afb42b5654aa9442c6136d4e576cab958
gunaifet2012 <gunaifet2012@gmail.com>
2026-09-09 13:15:33 +0530

    Add environment configuration in env.ts

---
+++ src/utils/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  adminUsername: process.env.ADMIN_USERNAME ?? 'Admin',
+  adminPassword: process.env.ADMIN_PASSWORD ?? 'admin123',
+  restrictedUsername: process.env.RESTRICTED_USERNAME,
+  restrictedPassword: process.env.RESTRICTED_PASSWORD
+};
